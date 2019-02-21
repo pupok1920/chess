106288ec003065da563f0151ab1bbcdc0e7c3aa5
pupok <adrenalunchik@rambler.ru>
2019-02-21 10:51:16 +0200

    [SRV] Code refactoring

--- src/pieces.h
+++ src/pieces.h
@@ -6,7 +6,7 @@ class Piece {
 public:
     Piece();
     virtual ~Piece();
-    virtual bool isMoveValid(int oldX, int oldY, int newX, int newY) const = 0;
+    virtual bool isMoveValid(int, int, int, int) const = 0;
 
     Piece& operator=(Piece const&) = delete;
     Piece(Piece const&) = delete;
@@ -15,37 +15,37 @@ public:
 class Pawn: public Piece {
 public:
     Pawn();
-    bool isMoveValid(int oldX, int oldY, int newX, int newY) const override;
+    bool isMoveValid(int, int, int, int) const override;
 };
 
 class Rook: public Piece {
 public:
     Rook();
-    bool isMoveValid(int oldX, int oldY, int newX, int newY) const override;
+    bool isMoveValid(int, int, int, int) const override;
 };
 
 class Knight: public Piece {
 public:
     Knight();
-    bool isMoveValid(int oldX, int oldY, int newX, int newY) const override;
+    bool isMoveValid(int, int, int, int) const override;
 };
 
 class Bishop: public Piece {
 public:
     Bishop();
-    bool isMoveValid(int oldX, int oldY, int newX, int newY) const override;
+    bool isMoveValid(int, int, int, int) const override;
 };
 
 class Queen: public Piece {
 public:
     Queen();
-    bool isMoveValid(int oldX, int oldY, int newX, int newY) const override;
+    bool isMoveValid(int, int, int, int) const override;
 };
 
 class King: public Piece {
 public:
     King();
-    bool isMoveValid(int oldX, int oldY, int newX, int newY) const override;
+    bool isMoveValid(int, int, int, int) const override;
 };
 
 #endif // __PIECESFJULVFJNSJKLSNJSKLVNVNSL__
